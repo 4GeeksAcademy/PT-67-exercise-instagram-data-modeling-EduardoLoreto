b0d4de9ea7a0030a9e37e513c3e218e5c796e062
EduardoLoreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-06-28 19:23:07 +0000

    Data Model Instagram

--- src/models.py
+++ src/models.py
@@ -7,30 +7,56 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Follower(Base):
+    __tablename__ = "follower"
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column (Integer,ForeignKey("user.id"), primary_key=True, nullable=False)
+    user_to_id = Column(Integer, ForeignKey("user.id"), primary_key=True, nullable=False)    
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = "user"
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250),nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ = "comment"
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250),nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+class Post(Base):
+    __tablename__ = "post"
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column (String(250), ForeignKey("user.id"), nullable=False)
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True,)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(String(250), ForeignKey("post.id"))
+
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 try:
-    result = render_er(Base, 'diagram.png')
+    result = render_er(Base, "diagram.png")
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
