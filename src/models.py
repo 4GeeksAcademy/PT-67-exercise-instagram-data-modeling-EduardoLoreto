cc512f36cf7af0d2df8dbc38e2671921a67b3e1d
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-07-01 18:35:13 +0000

    Finalizacion del Proyecto

--- src/models.py
+++ src/models.py
@@ -1,62 +1,57 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = "follower"
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    user_from_id = Column (Integer,ForeignKey("user.id"), primary_key=True, nullable=False)
-    user_to_id = Column(Integer, ForeignKey("user.id"), primary_key=True, nullable=False)    
-
-class User(Base):
-    __tablename__ = "user"
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250),nullable=False)
-    password = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    fistname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
-class Comment(Base):
-    __tablename__ = "comment"
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'Follower'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250),nullable=False)
-    password = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-
-
-class Post(Base):
-    __tablename__ = "post"
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    user_id = Column (String(250), ForeignKey("user.id"), nullable=False)
-
+    user_from_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('User.id'), nullable=False)
 
 class Media(Base):
-    __tablename__ = 'media'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True,)
-    type = Column(String(250), nullable=False)
-    url = Column(String(250), nullable=False)
-    post_id = Column(String(250), ForeignKey("post.id"))
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum, nullable=False)
+    url = Column(String(250), ForeignKey('User.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
 
+class Post(Base):
+    __tablename__ = 'Post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 try:
-    result = render_er(Base, "diagram.png")
+    result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
