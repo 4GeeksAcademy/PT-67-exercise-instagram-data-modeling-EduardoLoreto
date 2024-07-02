15a5024106fb69dd3d7bf2ad3ede437d7844143f
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-07-02 10:06:24 +0000

    Proyecto Terminado

--- src/models.py
+++ src/models.py
@@ -1,17 +1,11 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
-class Follower(Base):
-    __tablename__ = 'Follower'
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('User.id'), nullable=False)
-    user_to_id = Column(Integer, ForeignKey('User.id'), nullable=False) 
 
 class User(Base):
     __tablename__ = 'User'
@@ -23,13 +17,17 @@ class User(Base):
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
-class Comment(Base):
-    __tablename__ = 'Comment'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'Follower'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250), nullable=False)
-    author_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    user_from_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum, nullable=False)
+    url = Column(String(250), ForeignKey('User.id'), nullable=False)
     post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
 
 class Post(Base):
@@ -39,12 +37,13 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
 
-
-class Media(Base):
-    __tablename__ = 'Media'
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    type = Column(Enum, nullable=False)
-    url = Column(String(250), ForeignKey('User.id'), nullable=False)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('User.id'), nullable=False)
     post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
 
     def to_dict(self):
@@ -52,7 +51,7 @@ class Media(Base):
 
 ## Draw from SQLAlchemy base
 try:
-    result = render_er(Base, "diagram.png")
+    result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
